86e74ae5ac59be5fc1087f813bb2e41735728f8d
Raynard Brown <brown_raynard@icloud.com>
2024-09-23 14:25:04 -0500

    JavaScriptEx1

--- app.js
+++ app.js
@@ -5,6 +5,31 @@ console.log(
 
 // Exercise 1
 
+let lastName;
+let age;
+
 // Exercise 2
+ const firstName = "Raynard";
+ lastName = "Brown"
+ age = 43;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = "true";
+
+// Exercise 4
+let prrice = 19.99;
+let isOnSale = "false";
+let salePercentage = 15;
+let stock = 0;
+let selectedSize = "M";
+
+// Exercise 5
+`let title = "Name of the Wind"`;
+`let author = "Patrick Rothfuss"`;
+`let pageCount = 722`;
+`let bookmark = 456`;
+`let hasRead = "true"`;
+
+
